ecfb4da762fc6c6a20aa32f434e880d0c2284a37
yyyybbbb <1559068537@qq.com>
2016-07-12 10:41:15 -0700

    s4

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("title","slug","published","status",)
+	list_filter = ("status","created","published","author",)
+	search_fields = ("title","body",)
+	prepopulated_fields = {'slug': ('title',),}
+	raw_id_fields = ('author',)
+	date_hierarchy = "published"
+	ordering = ['status','published',]
+
+admin.site.register(Post,PostAdmin)
