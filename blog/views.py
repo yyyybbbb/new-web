21d7f755992cee7c106b8ffb4859806984332290
yyyybbbb <1559068537@qq.com>
2016-07-15 10:10:36 -0700

    2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from.models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(status="published")
+    return render(request, 'blog/post/list.html',{'posts':posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post,id=post_id)
+    return render(request, 'blog/post/detail.html',{'posts':posts})
+
+
 
 # Create your views here.
+
