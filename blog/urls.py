587706ce565b0c86f4b4a029ce0c027f0668bf9b
yyyybbbb <1559068537@qq.com>
2016-07-15 10:27:11 -0700

    4

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+     url(r'^$',view.post_list, name='post_list'),
+     url(r'^(?P<post_id>\d)/$', views.post_detail, name='post_detail'),
+]
